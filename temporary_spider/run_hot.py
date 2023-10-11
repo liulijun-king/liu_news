222c4bdb6ded65e22cc9db2246c601cf44d4afd6
liulijun <1143421302@qq.com>
2023-10-11 13:58:16 +0800

    关闭代理

--- temporary_spider/run_hot.py
+++ temporary_spider/run_hot.py
@@ -10,10 +10,7 @@ from header_spider.hot_web import HotWeb
 
 if __name__ == '__main__':
     producer = KafkaProducer(
-        bootstrap_servers=['140.210.203.161:9092', '140.210.219.168:9092', '140.210.207.185:9092'])
-    proxies = {
-        "http": "127.0.0.1:7890",
-        "https": "127.0.0.1:7890"
-    }
+        bootstrap_servers=['8.130.131.161:9092', '8.130.94.243:9092', '8.130.37.191:9092'])
+    proxies = None
     mp = HotWeb(is_proxies=proxies, kafka_pro=producer)
     mp.id_split()
