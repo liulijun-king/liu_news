abba3b2f36ca0f4cbba0c4206253aac4315c3c3f
liulijun <1143421302@qq.com>
2023-10-13 10:38:40 +0800

    布隆过滤器集群修改

--- GitHubAll/tools/bloom_filter.py
+++ GitHubAll/tools/bloom_filter.py
@@ -6,18 +6,26 @@
 import hashlib
 import redis
 from larkspur import ScalableBloomFilter
+from rediscluster import RedisCluster
 
-# from redis.cluster import RedisCluster
-# from redis.cluster import ClusterNode
+IS_CLUSTER = True
 
-# startup_nodes = [
-#     ClusterNode("47.97.216.52", 6379),
-#     ClusterNode("120.55.67.165", 6379),
-#     ClusterNode("120.26.85.177", 6379),
-# ]
-# redis_conn = RedisCluster(startup_nodes=startup_nodes, decode_responses=True, password='gew29YAyi')
-redis_conn = redis.StrictRedis(host='47.97.216.52', port=6379, db=0, socket_connect_timeout=15, decode_responses=False,
-                               password='gew29YAyi')
+if IS_CLUSTER:
+    startup_nodes = [
+        {"host": "47.97.216.52", "port": 6379},
+        {"host": "120.55.67.165", "port": 6379},
+        {"host": "120.26.85.177", "port": 6379}
+    ]
+    # 创建 Redis 集群连接
+    redis_conn = RedisCluster(
+        startup_nodes=startup_nodes,
+        decode_responses=False, socket_connect_timeout=30, password='gew29YAyi'
+    )
+else:
+    redis_conn = redis.StrictRedis(host='47.97.216.52', port=6379, db=0, socket_connect_timeout=15,
+                                   decode_responses=False,
+                                   password='gew29YAyi')
+
 sbf = ScalableBloomFilter(connection=redis_conn,
                           name='bloom_filter',
                           initial_capacity=100000000,
