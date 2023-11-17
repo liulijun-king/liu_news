a753ebf9f0e88ac51c197e6dd25ceaa880bde988
liulijun <1143421302@qq.com>
2023-11-17 10:27:32 +0800

    添加启动语句

---
+++ GitHubAll/start_spider.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 检查 Docker 容器是否已经启动
+if ! docker inspect -f '{{.State.Running}}' dazzling_bohr &>/dev/null; then
+    echo "Docker container is not running. Restarting..."
+    docker restart dazzling_bohr
+else
+    echo "Docker container is already running."
+fi
+
+# 进入 Docker 容器并执行 main.py 文件
+docker exec -it dazzling_bohr sh /data/start_all.sh
