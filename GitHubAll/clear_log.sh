4a9a31b9cfe30572469db63fd64f425ae08cefad
liulijun <1143421302@qq.com>
2023-11-23 14:40:41 +0800

    添加日志删除操作

--- GitHubAll/clear_log.sh
+++ GitHubAll/clear_log.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-log_path=/u01/isi/CenterProject/GitHubAll/
-#log_path=/home
-cd $log_path
-find $log_path -name "*.log" > .del.dat
-find $log_path -name "*.log.*" >> .del.dat
-find $log_path -name "nohup.out" >> .del.dat
-find $log_path -name "catalina.out" >> .del.dat
-for i in `cat .del.dat`
-do
-  cat /dev/null  > $i
-done
+#log_path=/u01/isi/CenterProject/GitHubAll/
+##log_path=/home
+#cd $log_path
+#find $log_path -name "*.log" > .del.dat
+#find $log_path -name "*.log.*" >> .del.dat
+#find $log_path -name "nohup.out" >> .del.dat
+#find $log_path -name "catalina.out" >> .del.dat
+#for i in `cat .del.dat`
+#do
+#  cat /dev/null  > $i
+#done
+
+find /data -type f -name "*.log" -exec rm {} \;
+find /data -type f -name "*.out" -exec rm {} \;
---
+++ GitHubAll/log_clear.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export LANG="zh_CN.UTF-8"
+
+docker exec dazzling_bohr  /bin/sh /data/clear_log.sh
