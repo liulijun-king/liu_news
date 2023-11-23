7ab0d98c479679fbb70f2b0bfb4f585000ed788f
liulijun <1143421302@qq.com>
2023-11-23 11:57:19 +0800

    测试项目采集

--- GitHubAll/id_task_add/case_redis.py
+++ GitHubAll/id_task_add/case_redis.py
@@ -22,4 +22,5 @@ redis_conn = RedisCluster(
     decode_responses=True, socket_connect_timeout=30, password='gew29YAyi'
 )
 
-print(redis_conn.llen(user_url_key))
+print(redis_conn.llen('github_all:item_info_handle'))
+print(redis_conn.llen('gb_all:item_info_task'))
