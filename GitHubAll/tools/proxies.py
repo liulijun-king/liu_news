f71f85a219efaeece19c9549031fedd931bea6f5
liulijun <1143421302@qq.com>
2023-11-24 15:58:09 +0800

    美国服务器和新加坡服务器不同

--- GitHubAll/tools/proxies.py
+++ GitHubAll/tools/proxies.py
@@ -30,4 +30,4 @@ def queue_empty():
     #             pro = "http://" + pro
     #         break
     # proxies = str(pro)
-    return "http://f2199815664-region-SG-period-0:k0sl96zx@as.r618.kdlfps.com:18866/"
+    return "http://f2199815664-region-US-period-0:k0sl96zx@us.r618.kdlfps.com:18866/"
