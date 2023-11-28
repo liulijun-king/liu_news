b5170df504676601f810c63722f5fa50435c3805
liulijun <1143421302@qq.com>
2023-11-28 17:55:31 +0800

    测试

--- temporary_spider/header_spider/header_spider.py
+++ temporary_spider/header_spider/header_spider.py
@@ -54,7 +54,7 @@ class HeadSpider(Base_spider):
         data_dict = self.module
         for key, value in data_dict.items():
             self.config = value
-            if "中国数字时代" in value.get("website_name"):
+            if "博讯" in value.get("website_name"):
                 self.history_spider(key)
 
     def id_split_thread(self):
