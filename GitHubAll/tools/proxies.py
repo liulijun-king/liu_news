556b15b8d25dfad4d62d4c3cd4aa82a925e0cc04
liulijun <1143421302@qq.com>
2023-11-28 09:57:20 +0800

    github切换代理

--- GitHubAll/tools/proxies.py
+++ GitHubAll/tools/proxies.py
@@ -30,4 +30,4 @@ def queue_empty():
     #             pro = "http://" + pro
     #         break
     # proxies = str(pro)
-    return "http://f2199815664-region-US-period-0:k0sl96zx@us.r618.kdlfps.com:18866/"
+    return "http://shengmingxing_zxcj-zone-custom:zxc_2023@679e234e86e0ed04.na.ipidea.online:2333"
