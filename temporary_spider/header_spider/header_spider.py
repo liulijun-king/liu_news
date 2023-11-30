4e5fe9f9a9f65b86fb5c048cc1358c2290eb55ce
liulijun <1143421302@qq.com>
2023-11-30 18:13:42 +0800

    测试

--- temporary_spider/header_spider/header_spider.py
+++ temporary_spider/header_spider/header_spider.py
@@ -55,7 +55,7 @@ class HeadSpider(Base_spider):
         data_dict = self.module
         for key, value in data_dict.items():
             self.config = value
-            if "三立新闻网" in value.get("website_name"):
+            if "台湾经济日报" in value.get("website_name"):
                 self.history_spider(key)
 
     def id_split_thread(self):
