c1c10069133e81b305d7c8f35937be9184388f0a
liulijun <1143421302@qq.com>
2023-12-15 15:40:28 +0800

    测试改回

--- temporary_spider/run_head.py
+++ temporary_spider/run_head.py
@@ -23,19 +23,15 @@ if __name__ == '__main__':
         startup_nodes=startup_nodes,
         decode_responses=True, socket_connect_timeout=30, password='gew29YAyi'
     )
-    print(redis_conn.sismember("key_news:pl", "https://www.rfi.fr/fr/afrique/20230111-en-visite-au-burkina-faso-sissoco-embalo-r%C3%A9affirme-le-soutien-de-la-c%C3%A9d%C3%A9ao"))
-    print(redis_conn.sismember("key_news:pl", "https://cn.wsj.com/articles/报道-私募股权投资公司thoma-bravo考虑收购twitter-11649979906"))
-    # producer = KafkaProducer(
-    #     bootstrap_servers=['172.22.100.90:9092'])
-    # hw_obs = MiniDown()
-    # # http://shengmingxing_zxcj-zone-custom:wzcj_2023@679e234e86e0ed04.na.ipidea.online:2333
-    # # "http://f2199815664-region-SG-period-0:k0sl96zx@as.r618.kdlfps.com:18866/"
-    # proxies = {
-    #     'http': "http://f2199815664-region-SG-period-0:k0sl96zx@as.r618.kdlfps.com:18866/",
-    #     'https': "http://f2199815664-region-SG-period-0:k0sl96zx@as.r618.kdlfps.com:18866/"
-    # }
-    # mp = HeadSpider(is_proxies=proxies, hw_db=hw_obs, kafka_pro=producer, redis_conn=redis_conn)
-    # # mp.id_split()
-    # mp.id_split_thread()
-
-
+    producer = KafkaProducer(
+        bootstrap_servers=['172.22.100.90:9092'])
+    hw_obs = MiniDown()
+    # http://shengmingxing_zxcj-zone-custom:wzcj_2023@679e234e86e0ed04.na.ipidea.online:2333
+    # "http://f2199815664-region-SG-period-0:k0sl96zx@as.r618.kdlfps.com:18866/"
+    proxies = {
+        'http': "http://f2199815664-region-SG-period-0:k0sl96zx@as.r618.kdlfps.com:18866/",
+        'https': "http://f2199815664-region-SG-period-0:k0sl96zx@as.r618.kdlfps.com:18866/"
+    }
+    mp = HeadSpider(is_proxies=proxies, hw_db=hw_obs, kafka_pro=producer, redis_conn=redis_conn)
+    # mp.id_split()
+    mp.id_split_thread()
