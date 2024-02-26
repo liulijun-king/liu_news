661e52165b2c9430366135c74224c868424e421d
liulijun <1143421302@qq.com>
2024-02-26 11:03:18 +0800

    修改kafka

--- temporary_spider/run_hot.py
+++ temporary_spider/run_hot.py
@@ -10,7 +10,7 @@ from header_spider.hot_web import HotWeb
 
 if __name__ == '__main__':
     producer = KafkaProducer(
-        bootstrap_servers=['172.22.100.90:9092'])
+        bootstrap_servers=['8.130.131.161:9092','8.130.94.243:9092','8.130.37.191:9092'])
     # http://liulijun584268-zone-custom:9TL39WvUnboIdOI@9662f10ce723ec40.na.ipidea.online:2333
     proxies = {
         'http': "http://f2199815664-region-SG-period-0:k0sl96zx@as.r618.kdlfps.com:18866/",
